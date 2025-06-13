acdb91e95f437a5abfb1ee46fa362b2b38cd1d04
Naresh Khatri <nareshkhatri9810@gmail.com>
2025-06-13 14:22:52 +0545

    feat: enable CORS for frontend access

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import { connectDB } from './lib/db.js';
@@ -13,6 +14,10 @@ const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser())
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true, // allowed to frontend to send cookies
+}))
 
 app.use("/api/auth",AuthRoute );
 app.use("/api/user", userRoute);
